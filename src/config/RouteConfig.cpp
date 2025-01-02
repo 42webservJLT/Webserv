188dadbeb76689ab5a564c3a315aa04d9053a57c
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-02 18:57:08 +0100

    added initial methods for handling different lines of route config (not tested or verified yet); also validity of unmarshalled struc testing missing still

--- src/config/RouteConfig.cpp
+++ src/config/RouteConfig.cpp
@@ -1,5 +1,14 @@
 #include "RouteConfig.hpp"
 
+bool _handleRoot(std::string& line, std::string& root);
+bool _handleIndex(std::string& line, std::vector<std::string>& index);
+bool _handleAllowedMethods(std::string& line, std::set<std::string>& allowedMethods);
+bool _handleAutoindex(std::string& line, bool& autoindex);
+bool _handleRedirect(std::string& line, std::string& redirect);
+bool _handleCgiExtensions(std::string& line, std::map<std::string, std::string>& cgiExtensions);
+bool _handleUploadDir(std::string& line, std::string& uploadDir);
+bool _handleClientMaxBodySize(std::string& line, size_t& clientMaxBodySize);
+
 /* ----------------------------------------------------------------------------------- */
 /* RouteConfig Constructor & Destructor                                                */
 /* ----------------------------------------------------------------------------------- */
@@ -65,5 +74,194 @@ bool RouteConfig::Unmarshall(std::string& str) {
 	size_t clientMaxBodySize = DEFAULT_CLIENT_MAX_BODY_SIZE; // optional
 
 	std::stringstream ss(str);
+	std::string line;
+	while (std::getline(ss, line)) {
+		if (line.empty()) {
+			continue;
+		} else if (line == "}") {
+			break;
+		}
+
+		if (line.size() >= 5 && line.substr(0, 5) == "root ") {
+			if (!_handleRoot(line, root)) {
+				return false;
+			}
+		} else if (line.size() >= 6 && line.substr(0, 6) == "index ") {
+			if (!_handleIndex(line, index)) {
+				return false;
+			}
+		} else if (line.size() >= 15 && line.substr(0, 15) == "allowed_methods ") {
+			if (!_handleAllowedMethods(line, allowedMethods)) {
+				return false;
+			}
+		} else if (line.size() >= 10 && line.substr(0, 10) == "autoindex ") {
+			if (!_handleAutoindex(line, autoindex)) {
+				return false;
+			}
+		} else if (line.size() >= 9 && line.substr(0, 9) == "redirect ") {
+			if (!_handleRedirect(line, redirect)) {
+				return false;
+			}
+		} else if (line.size() >= 13 && line.substr(0, 13) == "cgi_extension ") {
+			if (!_handleCgiExtensions(line, cgiExtensions)) {
+				return false;
+			}
+		} else if (line.size() >= 11 && line.substr(0, 11) == "upload_dir ") {
+			if (!_handleUploadDir(line, uploadDir)) {
+				return false;
+			}
+		} else if (line.size() >= 19 && line.substr(0, 19) == "client_max_body_size ") {
+			if (!_handleClientMaxBodySize(line, clientMaxBodySize)) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 }
 
+// handles the root line
+bool _handleRoot(std::string& line, std::string& root) {
+	// split line containing root by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() != 2) {
+		return false;
+	}
+	root = tokens[1];
+	return true;
+}
+
+// handles the index line
+bool _handleIndex(std::string& line, std::vector<std::string>& index) {
+	// split line containing index by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() < 2) {
+		return false;
+	}
+	for (size_t i = 1; i < tokens.size(); i++) {
+		index.push_back(tokens[i]);
+	}
+	return true;
+}
+
+// handles the allowed_methods line
+bool _handleAllowedMethods(std::string& line, std::set<std::string>& allowedMethods) {
+	// split line containing allowed_methods by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() < 2) {
+		return false;
+	}
+	for (size_t i = 1; i < tokens.size(); i++) {
+		allowedMethods.insert(tokens[i]);
+	}
+	return true;
+}
+
+// handles the autoindex line
+bool _handleAutoindex(std::string& line, bool& autoindex) {
+	// split line containing autoindex by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() != 2) {
+		return false;
+	}
+	if (tokens[1] == "on") {
+		autoindex = true;
+	} else if (tokens[1] == "off") {
+		autoindex = false;
+	} else {
+		return false;
+	}
+	return true;
+}
+
+// handles the redirect line
+bool _handleRedirect(std::string& line, std::string& redirect) {
+	// split line containing redirect by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() != 2) {
+		return false;
+	}
+	redirect = tokens[1];
+	return true;
+}
+
+// handles the cgi_extensions line
+bool _handleCgiExtensions(std::string& line, std::map<std::string, std::string>& cgiExtensions) {
+	// split line containing cgi_extensions by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() < 3 || tokens.size() % 2 != 1) {
+		return false;
+	}
+	for (size_t i = 1; i < tokens.size(); i += 2) {
+		cgiExtensions[tokens[i]] = tokens[i + 1];
+	}
+	return true;
+}
+
+// handles the upload_dir line
+bool _handleUploadDir(std::string& line, std::string& uploadDir) {
+	// split line containing upload_dir by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() != 2) {
+		return false;
+	}
+	uploadDir = tokens[1];
+	return true;
+}
+
+// handles the client_max_body_size line
+bool _handleClientMaxBodySize(std::string& line, size_t& clientMaxBodySize) {
+	// split line containing client_max_body_size by space chars
+	std::istringstream iss(line);
+	std::vector<std::string> tokens;
+	std::string token;
+	while (std::getline(iss, token, ' ')) {
+		tokens.push_back(token);
+	}
+	if (tokens.size() != 2) {
+		return false;
+	}
+
+	try {
+		clientMaxBodySize = std::stoi(tokens[1]);
+	} catch (std::exception& e) {
+		return false;
+	}
+
+	return true;
+}
